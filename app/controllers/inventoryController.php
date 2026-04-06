f6f677755ffa00a5453be1875bf000b647e718ab
yasheng03 <yashengunasena@gmail.com>
2026-04-06 16:37:02 +0530

    Recovered local changes

--- app/controllers/inventoryController.php
+++ app/controllers/inventoryController.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!defined('ROOT_PATH')) {
     require_once __DIR__ . '/../../config/paths.php';  // ✅ correct
 }
